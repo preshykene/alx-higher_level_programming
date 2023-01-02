f281c4154aee93c04782481e4791e20e593c3b21
preshykene <preciousekene51@gmail.com>
2023-01-01 20:49:24 -0800

    Tried Updating the files

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,54 +1,46 @@
 #!/usr/bin/python3
-"""Unittest for max_integer([..])
-"""
 import unittest
 max_integer = __import__('6-max_integer').max_integer
-
-
 class TestMaxInteger(unittest.TestCase):
 
-    """run test with python3 -m unittest -v tests.6-max_integer_test"""
+    def test_one(self):
+        matrix1 = [1,2,3,4]
+        self.assertEqual(max_integer(matrix1),4)
 
-    def test_module_docstring(self):
-        moduleDoc = __import__('6-max_integer').__doc__
-        self.assertTrue(len(moduleDoc) > 1)
+    def test_two(self):
+        matrix2 = []
+        self.assertEqual(max_integer(matrix2),None)
 
-    def test_function_docstring(self):
-        functionDoc = __import__('6-max_integer').max_integer.__doc__
-        self.assertTrue(len(functionDoc) > 1)
+    def test_three(self):
+        self.assertRaises(TypeError, max_integer,0)
 
-    def test_signed_ints_and_floats(self):
-        self.assertEqual(max_integer([0]), 0)
-        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
-        self.assertEqual(max_integer([1, 2, 3, -4]), 3)
-        self.assertEqual(max_integer([-1.5, -2.5]), -1.5)
-        self.assertEqual(max_integer([10, -10, 10]), 10)
-        self.assertEqual(max_integer([{1, 9}, {2}, {3}]), {1, 9})
+    def test_four(self):
+        matrix4 = {5:'a',4:'b',3:'c'}
+        self.assertRaises(KeyError, max_integer,matrix4)
 
-    def test_list_of_strings(self):
-        self.assertEqual(max_integer("6789"), '9')
-        self.assertEqual(max_integer("abcxyz"), 'z')
-        self.assertEqual(max_integer(['a', 'b', 'c', 'x', 'y', 'z']), 'z')
-        self.assertEqual(max_integer(["abc", 'x']), 'x')
+    def test_five(self):
+        matrix = ['a','b','c']
+        self.assertEqual(max_integer(matrix),'c')
 
-    def test_lists(self):
-        self.assertEqual(max_integer([[1, 2], [1, 3]]), [1, 3])
+    def test_six(self):
+        matrix = 'hello'
+        self.assertEqual(max_integer(matrix),'o')
 
-    def test_other_sequences(self):
-        with self.assertRaises(TypeError):
-            max_integer({1, 2}, {3, 4, 5})
-        with self.assertRaises(TypeError):
-            max_integer({1, 2, 3, 4, 5})
-        with self.assertRaises(TypeError):
-            max_integer([-10, 0.5, "str", {1, 2}])
-        with self.assertRaises(TypeError):
-            max_integer([None, True])
+    def test_seven(self):
+        matrix = [-500,1,2,3]
+        self.assertEqual(max_integer(matrix),3)
 
-    def test_None(self):
-        self.assertIsNone(max_integer([]), None)
-        self.assertIsNone(max_integer(), None)
-        self.assertIsNone(max_integer([None]), None)
+    def test_eight(self):
+        matrix1 = [4]
+        self.assertEqual(max_integer(matrix1),4)
 
+    def test_nine(self):
+        matrix1 = [4,1,2,3]
+        self.assertEqual(max_integer(matrix1),4)
 
-if __name__ == "__main__":
+    def test_ten(self):
+        matrix1 = [1,2,6,5,4]
+        self.assertEqual(max_integer(matrix1),6)
+
+if __name__ == '__main__':
     unittest.main()
